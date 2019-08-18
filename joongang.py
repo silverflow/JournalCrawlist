a58335753519826d7d469b8201455022279cdf4d
metafor <silveryflow@naver.com>
2019-08-19 01:41:14 +0900

    중앙일보 기자페이지 크롤링

---
+++ joongang.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup
+import requests as rq
+'''
+중앙일보 기자 목록 페이지를 파싱해서 이름과 이메일을 가져오는 프로그램
+'''
+#함수 정의
+def get_pages(bs):
+    return bs.select('body .bd ul.list li')
+
+def data_parse(pages):
+    for page in pages:
+        name = page.find('h2').text.strip()
+    email = page.find('span', class_="icon_email").text.strip()
+    print(name,email)
+
+
+url= 'https://news.joins.com/Reporter'
+page_path='?page=%d'
+number = 2
+
+res = rq.get(url)
+bs = BeautifulSoup(res.content, 'lxml')
+pages = get_pages(bs)
+data_parse(pages)
+
+
+while True:
+    sub_path = page_path%(number)
+    number += 1
+    res = rq.get(url + sub_path)
+
+    bs = BeautifulSoup(res.content, 'lxml')
+    pages = get_pages(bs)
+
+    if(len(bs.select('p.no_result'))>0):
+        print("수집종료\n페이지가 끝남")
+        break
+    data_parse(pages)
