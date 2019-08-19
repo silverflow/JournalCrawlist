dd2fecca9e111bc316d6f6dd50dcc38d6ef85612
silveryflow <silveryflow@naver.com>
2019-08-19 15:16:05 +0900

    중앙일보 기자목록 페이지 크롤링하고 파싱해서 DB에 저장까지

--- joongang.py
+++ joongang.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests as rq
+import DBConn
 '''
 중앙일보 기자 목록 페이지를 파싱해서 이름과 이메일을 가져오는 프로그램
 '''
@@ -12,7 +13,14 @@ def data_parse(pages):
     for page in pages:
         name = page.find('h2').text.strip()
         email = page.find('span', class_="icon_email").text.strip()
+        curs.execute(sql, (name, email,"중앙일보"))
         print(name,email)
+#DB 커넥트 정보
+conn = DBConn.conn()
+#conect에서 cursor 생성
+curs = conn.cursor()
+#SQL문 실행 이름, 이메일, 언론사에 인서트하는 쿼리문
+sql = ("INSERT INTO `journalistinfo` (`journal_name`, `journal_email`,`press`) VALUES (%s,%s,%s)")
 
 url= 'https://news.joins.com/Reporter'
 page_path='?page=%d'
@@ -26,6 +34,7 @@ data_parse(pages)
 
 while True:
     sub_path = page_path%(number)
+    #다음 페이지로 넘버링
     number += 1
     res = rq.get(url + sub_path)
 
@@ -34,5 +43,9 @@ while True:
 
     if(len(bs.select('p.no_result'))>0):
         print("수집종료\n페이지가 끝남")
+        #DB에 데이터 쓰기
+        conn.commit()
+        #DB 커넥트 닫기
+        conn.close()
         break
     data_parse(pages)
